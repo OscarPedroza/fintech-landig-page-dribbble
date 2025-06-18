0d3b4b3a63451163b51939e57139d307f43e682e
Oscar Pedroza <oscarph1802@gmail.com>
2025-06-17 21:05:15 -0600

    Menu fijo

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 'use strict';
 
+/**
+ * RESPONSIVE MENU
+ */
+
 const btnToggle = document.getElementById('btn-toggle');
 const menu = document.getElementById('menu');
 
 btnToggle.addEventListener('click', () => {
   menu.classList.toggle('active');
   btnToggle.classList.toggle('active');
+});
+
+/**
+ * FIXED MENU
+ */
+
+window.addEventListener('scroll', () => {
+  const header = document.getElementById('header');
+  header.classList.toggle('fixed', window.scrollY > 80);
 });
